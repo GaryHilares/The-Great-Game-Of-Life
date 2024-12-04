017410d3bd04e2d3ab107d87a69ad3bdfb600951
Gary Hilares <dev.garyhilares@gmail.com>
2024-12-04 14:41:58 -0800

    Added styles to counter view

--- src/TimeCounterView.tsx
+++ src/TimeCounterView.tsx
@@ -1,6 +1,8 @@
+import styles from "./TimeCounterView.module.scss";
+
 function TimeCounterView({ seconds }: { seconds: number | null }) {
     return (
-        <div>
+        <div className={styles.wrapper}>
             <p>Time until next update: {seconds ? seconds : 0}</p>
         </div>
     );
